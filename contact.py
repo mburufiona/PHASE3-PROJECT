b640f26e95375f46cc0155a1f9315d05a43c8b79
fiona <fiona.mburu@student.moringaschool.com>
2024-09-18 20:58:24 +0300

    adds an edit and delete function to the contact book

--- contact.py
+++ contact.py
@@ -2,31 +2,38 @@ import sqlite3
 
 def add_contact(name, phone, email):
     conn = sqlite3.connect('contacts.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO contacts (name, phone, email) VALUES (?, ?, ?)", (name, phone, email))
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO contacts (name, phone, email) VALUES (?, ?, ?)", (name, phone, email))
     conn.commit()
     conn.close()
     
 
 def view_contacts():
     conn = sqlite3.connect('contacts.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM contacts")
-    contacts = c.fetchall()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM contacts")
+    contacts = cursor.fetchall()
     conn.close()
     return contacts
 
 def search_contact(name):
     conn = sqlite3.connect('contacts.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM contacts WHERE name = ?", ('%' +name, + '%'))
-    contact = c.fetchall()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM contacts WHERE name = ?", ('%' +name, + '%'))
+    contact = cursor.fetchall()
     conn.close()
     return contact
 
 def edit_contact(contact_id, name, phone, email):
     conn = sqlite3.connect('contacts.db')
-    c = conn.cursor()
-    c.execute("UPDATE contacts SET name = ?, phone = ?, email = ? WHERE id = ?", (name, phone, email, contact_id))
+    cursor = conn.cursor()
+    cursor.execute("UPDATE contacts SET name = ?, phone = ?, email = ? WHERE id = ?", (name, phone, email, contact_id))
+    conn.commit()
+    conn.close()
+
+def delete_contact(contact_id):
+    conn = sqlite3.connect('contacts.db')
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM contacts WHERE id = ?', (contact_id))
     conn.commit()
     conn.close()
